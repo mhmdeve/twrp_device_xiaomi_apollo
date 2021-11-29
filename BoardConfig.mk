493f07417493132e5a2bf2a477fdb4c43c850131
Master <mhmdeveloperofficial@gmail.com>
2021-11-29 08:08:09 +0000

    apollo: libinit: Import changes from sm8150-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,7 @@
 #
 
 ALLOW_MISSING_DEPENDENCIES := true
+DEVICE_PATH := device/xiaomi/apollo
 
 # Architecture
 TARGET_ARCH := arm64
@@ -43,6 +44,10 @@ TARGET_USES_UEFI := true
 TARGET_BOARD_PLATFORM := kona
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno650
 QCOM_BOARD_PLATFORMS += kona
+
+# Init
+TARGET_INIT_VENDOR_LIB ?= //$(DEVICE_PATH):init_xiaomi_kona
+TARGET_RECOVERY_DEVICE_MODULES ?= init_xiaomi_kona
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 cgroup.memory=nokmem,nosocket
